ec407594523c6304f3ada64154f5c37702a8aea0
CJBiohacker <cj.moyses@gmail.com>
2021-08-16 14:44:00 -0300

    "Imutabilidade no JS"

--- JavaScript/conceitos/imutabilidade.js
+++ JavaScript/conceitos/imutabilidade.js
@@ -12,11 +12,3 @@ function getUserFullName() {
 const fullName = getUserFullName();
 
 console.log(fullName);
-
-// Uso do Spread Operador ou Spread Syntax '...'. É utilizado quando é necessário copiar os itens de uma array, sem ter de modificá-la.
-
-let monstros = ["Ogro", "Goblin", "Hydra", "Dragão", "Ímpio"]       // Declaração da Array 'monstros' e atribuição de strings.
-let criaturas = ["Humano", ...monstros, "Vampiro", "Elfo"]          // Declaração da Array 'criaturas' e atribuição de strings + uso do Spread Operator para copiar todos elementos de 'monstros'.
-
-console.log(criaturas);
-console.log(criaturas.join(' - ').toString())
