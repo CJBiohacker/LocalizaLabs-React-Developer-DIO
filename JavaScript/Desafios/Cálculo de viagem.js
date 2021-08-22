48a4fbb05bf8fa12405cd7326ca2023112b44a5a
CJBiohacker <cj.moyses@gmail.com>
2021-08-22 01:29:57 -0300

    "Desafio JavaSCript"

---
+++ JavaScript/Desafios/Cálculo de viagem.js
@@ -0,0 +1,11 @@
+// Entradas
+var tempo = 10;
+var velMedia = 85;
+
+const consumo = 12;
+const dist = velMedia * tempo;
+
+var combust = dist / consumo;
+
+// Saída
+console.log(combust.toFixed(3));
