a29b4347d8b8c441baf0f30f85bde51ae2403672
CJBiohacker <cj.moyses@gmail.com>
2021-08-23 02:39:28 -0300

    "Desafios JavaScript"

---
+++ JavaScript/Desafios/Programa para Validação de Notas.js
@@ -0,0 +1,47 @@
+const isValid = value => value <= 10 && value >= 0;
+
+let getout = false;
+
+let numA = null;
+let numB = null;
+
+while (x == 2) {
+    const num = Number(lines.shift());
+
+    if (           ) {
+        console.log("nota invalida");
+        continue;
+    }
+
+    if (           ) {
+
+
+    }
+
+    numB = num;
+
+    const average = (numA + numB) / 2
+
+    let validResponse = false;
+
+    numA = null;
+    numB = null;
+
+    console.log("media = " + average.toFixed(2));
+
+    let response = null;
+
+    do {
+        console.log("novo calculo (1-sim 2-nao)");
+
+        response = lines.shift();
+
+        switch (response) {
+            case "1":
+
+
+            case "2":
+
+        }
+    } while (               );
+}
