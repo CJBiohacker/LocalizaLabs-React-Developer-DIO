69933d9ec23fefae0bab2668bdc78341b39d24ba
CJBiohacker <cj.moyses@gmail.com>
2021-08-22 23:35:25 -0300

    "Symbols e Iterators no JS ES6"

---
+++ JavaScript/conceitos/symbols-e-iterators.js
@@ -0,0 +1,26 @@
+// SYMBOLS E ITERATORS NO JAVASCRIPT ES6.
+
+// Symbol é tipo de dado primitivo no JavaScript e surgiu na versão ES6.
+const uniqueID = Symbol("Isto é um Symbol.");       // Declaração de um Symbol.
+
+console.log(uniqueID);
+
+const array = [1, 2, 3, 4];
+const strings = 'Symbols e Iterators no JS';
+const array2 = [...array, ...strings];
+const iterador = array[Symbol.iterator]();          // Método de iteração de um Symbol, semelhante ao 'for...of...'.
+
+console.log("\nMétodo 'Iterator' de um Symbol");
+for (let i = 0; i < array.length; i++) {            // Estrutura de Repetição para iteração e impressão de cada valor da array como Symbol.
+    console.log(iterador.next());
+}
+
+console.log("\nEstrutura de Repetição 'For...of...' para repetição dos elementos da 'array'");
+for (let element of array) {                        // Estrutura de Repetição dos elementos da array.
+    console.log(element);
+}
+
+console.log("\nEstrutura de Repetição 'For...of...' para repetição dos elementos da 'array2'");
+for (const element of array2) {
+    console.log(element);
+}
