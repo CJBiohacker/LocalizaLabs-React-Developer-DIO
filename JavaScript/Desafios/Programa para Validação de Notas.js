556c8ce33979bfbc12731f15f75300c9d8181ee6
CJBiohacker <cj.moyses@gmail.com>
2021-08-25 18:18:56 -0300

    "Atualização Desafio Avançado JS"

--- JavaScript/Desafios/Programa para Validação de Notas.js
+++ JavaScript/Desafios/Programa para Validação de Notas.js
@@ -1,47 +1,5 @@
-const isValid = value => value <= 10 && value >= 0;
+const isValid = (value) => value <= 10 && value >= 0;           // Define o intervalo de entrada da nota
 
-let getout = false;
+var nota1;
+var nota2;
 
-let numA = null;
-let numB = null;
-
-while (x == 2) {
-    const num = Number(lines.shift());
-
-    if (           ) {
-        console.log("nota invalida");
-        continue;
-    }
-
-    if (           ) {
-
-
-    }
-
-    numB = num;
-
-    const average = (numA + numB) / 2
-
-    let validResponse = false;
-
-    numA = null;
-    numB = null;
-
-    console.log("media = " + average.toFixed(2));
-
-    let response = null;
-
-    do {
-        console.log("novo calculo (1-sim 2-nao)");
-
-        response = lines.shift();
-
-        switch (response) {
-            case "1":
-
-
-            case "2":
-
-        }
-    } while (               );
-}
