76299b493e03668ff5894bc628b789e61e27716d
CJBiohacker <cj.moyses@gmail.com>
2021-09-05 12:28:07 -0300

    "Aplicando o Button"

--- React/Introducao/react-teste/src/App.js
+++ React/Introducao/react-teste/src/App.js
@@ -1,9 +1,15 @@
-import Button from './Components/Button'
+// Components
+import ButtonCJ from './Components/Button_CJ'
+import ButtonTheBoss from './Components/Button_TheBoss'
+
+// Assets
 import logo from './Assets/logo.svg';
+
+// Styles
 import './Styles/App.css';
 
 const sum = (a, b) => {
-  alert(a + b);
+  alert(`Idade => ${a + b} anos`);
 }
 
 function App() {
@@ -13,7 +19,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Olá Mundo FilhadaMãe !!!
         </p>
-        <Button onClick={() => sum(10, 5)} name="Carlos Junior" />
+        <ButtonCJ onClick={() => sum(20, 11)} name="Carlos Junior" />
+        <ButtonTheBoss onClick={() => sum(20, 22)} name="The Boss" />
         <a
           className="App-link"
           href="https://reactjs.org"
