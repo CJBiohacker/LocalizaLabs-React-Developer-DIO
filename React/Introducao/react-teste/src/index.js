51c37cb484527d0df30723c7d5cafcecfb0bd127
CJBiohacker <cj.moyses@gmail.com>
2021-09-05 20:07:36 -0300

    " Index de vínculo entre HTML e o código do App"

--- React/Introducao/react-teste/src/index.js
+++ React/Introducao/react-teste/src/index.js
@@ -6,4 +6,4 @@ import App from './App';
 
 const idRoot = document.getElementById('root');                               // Atribui o método DOM 'getElementById' à uma constante chamada 'idRoot'.
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, idRoot);         // Chamada da constante 'idRoot' para renderização de toda atualização no arquivo 'App.js' seja lançada na página HTML em tempo real (S.P.A.).
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, idRoot);        // Chamada da constante 'idRoot' para renderização de toda atualização no arquivo 'App.js' seja lançada na página HTML em tempo real (S.P.A.).
