c2df1afc57bb642d05415fe198769743f530ae82
CJBiohacker <cj.moyses@gmail.com>
2021-09-05 11:54:31 -0300

    " Componente Botão "

---
+++ React/Introducao/react-teste/src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = (props) => {
+
+    const { name, onClick } = props                     // Desestruturação pra atribuir 2 atributos pra constante 'props' que será um callback do componente 'Button'.
+
+    return (
+        <button onClick={onClick}>{name}</button>       // Chamada da propriedade 'onClick' da tag button, pra passar o atributo 'onClick' como funcionalidade de clicar e 'name' como informação.
+    )
+}
+
+export default Button
