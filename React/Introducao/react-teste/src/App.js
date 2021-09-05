5949237c8a08f7bbacea7c82ec064c50758f36c9
CJBiohacker <cj.moyses@gmail.com>
2021-09-05 20:06:52 -0300

    " Renderização Condictional "

--- React/Introducao/react-teste/src/App.js
+++ React/Introducao/react-teste/src/App.js
@@ -1,6 +1,6 @@
 // Components
-import ButtonCJ from './Components/Button_CJ'
-import ButtonTheBoss from './Components/Button_TheBoss'
+import ButtonCJ from './Components/Button_CJ';
+import ButtonTheBoss from './Components/Button_TheBoss';
 
 // Assets
 import logo from './Assets/logo.svg';
@@ -8,8 +8,22 @@ import logo from './Assets/logo.svg';
 // Styles
 import './Styles/App.css';
 
+const condition = true;
+
 const sum = (a, b) => {
   alert(`Idade => ${a + b} anos`);
+}
+
+const ifTheBoss = () => {
+  return (
+    <ButtonTheBoss onClick={() => sum(20, 22)} name="The Boss" />
+  )
+}
+
+const ifCJ = () => {
+  return (
+    <ButtonCJ onClick={() => sum(20, 11)} name="Carlos Junior" />
+  )
 }
 
 const App = () => {
@@ -18,11 +32,12 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Clique nos botões para saber a idade:</p>
-        <ButtonCJ onClick={() => sum(20, 11)} name="Carlos Junior" />
-        <ButtonTheBoss onClick={() => sum(20, 22)} name="The Boss" />
+
+        {condition ? ifCJ() : ifTheBoss()}                                       {/* If Ternário para selecionar qual dos botões fica oculto. Condition: True => Botão CJ visível / False => Botão TheBoss visível */}
+
       </header>
     </div>
   );
-}
+};
 
 export default App;
