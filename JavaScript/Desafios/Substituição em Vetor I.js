b4392d2fa0091c4b9070682962e5f299bc63f50d
CJBiohacker <cj.moyses@gmail.com>
2021-09-09 16:08:55 -0300

    "Desafio Artimético JS Intermediário 3/3"

---
+++ JavaScript/Desafios/Substituição em Vetor I.js
@@ -0,0 +1,12 @@
+const input = [-78945, -260, 6, -5, -294, 950, -50000, -30645, 0, -583]
+
+for (let i = 0; i < 10; i++) {
+    console.log(`X[${i}] =  ${input[i] <= 0 ? 1 : input[i]}`);
+}
+
+// // CÓDIGO DEFAULT DA DIGITAL INNOVATION ONE
+// const input = gets();
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`X[${ i }] = ${ n[i] <= 0 ? 1 : n[i] } `);
+// }
