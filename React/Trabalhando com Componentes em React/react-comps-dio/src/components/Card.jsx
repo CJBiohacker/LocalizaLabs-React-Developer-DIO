dc1dcb473897561141a21339c4e0f211cc54232f
CJBiohacker <cj.moyses@gmail.com>
2021-09-09 22:01:42 -0300

    " Aplicação de Estado do React (useState) "

--- React/Trabalhando com Componentes em React/react-comps-dio/src/components/Card.jsx
+++ React/Trabalhando com Componentes em React/react-comps-dio/src/components/Card.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
+import { useState } from 'react'
 
 import '../styles/Card.css'
 
 const Card = (props) => {
 
+    const [greenValue, setGreenValue] = useState(0)
+    const [redValue, setRedValue] = useState(0)
+
+    const Like = () => {
+        setGreenValue(greenValue + 1)
+    }
+
+    const Dislike = () => {
+        setRedValue(redValue - 1)
+    }
+
     const { title, subtitle, text, greenButtonText, redButtonText } = props
 
     return (
 
-        <div class="card">
-            <h5 class="card-header">{title}</h5>
+        <div className="card">
+            <h5 className="card-header">{title}</h5>
             <div id='card-container'>
-                <div class="card-body">
-                    <h5 class="card-title">{subtitle}</h5>
-                    <p class="card-text">{text}</p>
-                    <span class='buttons'>
-                        <button type="button" class="btn btn-outline-success">{greenButtonText}</button>
-                        <button type="button" class="btn btn-outline-danger">{redButtonText}</button>
+                <div className="card-body">
+                    <h5 className="card-title">{subtitle}</h5>
+                    <p className="card-text">{text}</p>
+                    <span className='buttons'>
+                        <button type="button"
+                            className="btn btn-outline-success"
+                            onClick={Like}>{greenButtonText} <img src="https://img.icons8.com/material-rounded/24/000000/facebook-like--v1.png" /> {greenValue}</button>
+                        <button type="button"
+                            className="btn btn-outline-danger"
+                            onClick={Dislike}>{redButtonText} <img src="https://img.icons8.com/material-rounded/24/000000/thumbs-down.png" /> {redValue}</button>
                     </span>
                 </div>
             </div>
