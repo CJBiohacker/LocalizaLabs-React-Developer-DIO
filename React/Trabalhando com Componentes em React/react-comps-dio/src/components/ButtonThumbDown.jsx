b0c59b818ae13885a3d3611daa73d270af944fc5
CJBiohacker <cj.moyses@gmail.com>
2021-09-09 22:49:36 -0300

    "Componente Botão Dislike"

--- React/Trabalhando com Componentes em React/react-comps-dio/src/components/ButtonThumbDown.jsx
+++ React/Trabalhando com Componentes em React/react-comps-dio/src/components/ButtonThumbDown.jsx
@@ -4,7 +4,6 @@ const ButtonThumbDown = (props) => {
 
     const { text, children, onClick, className } = props
 
-
     return (
         <button type="button" onClick={onClick} class="btn btn-outline-danger" >{text}
             &nbsp;<img src="https://img.icons8.com/material-rounded/24/000000/thumbs-down.png" />&nbsp;{children}</button >
