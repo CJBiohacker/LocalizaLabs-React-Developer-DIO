d34f594468b3d2b2513c217928e1c635ea5bde2c
CJBiohacker <cj.moyses@gmail.com>
2021-09-09 21:32:40 -0300

    "Compoentente Card de Seleção"

--- React/Trabalhando com Componentes em React/react-comps-dio/src/components/Card.jsx
+++ React/Trabalhando com Componentes em React/react-comps-dio/src/components/Card.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
+import '../styles/Card.css'
+
 const Card = (props) => {
 
-    const { title, subtitle, text, buttonText } = props
+    const { title, subtitle, text, greenButtonText, redButtonText } = props
 
     return (
 
         <div class="card">
             <h5 class="card-header">{title}</h5>
-            <div class="card-body">
-                <h5 class="card-title">{subtitle}</h5>
-                <p class="card-text">{text}</p>
-                <a href="#" class="btn btn-primary">{buttonText}</a>
+            <div id='card-container'>
+                <div class="card-body">
+                    <h5 class="card-title">{subtitle}</h5>
+                    <p class="card-text">{text}</p>
+                    <span class='buttons'>
+                        <button type="button" class="btn btn-outline-success">{greenButtonText}</button>
+                        <button type="button" class="btn btn-outline-danger">{redButtonText}</button>
+                    </span>
+                </div>
             </div>
         </div>
 
