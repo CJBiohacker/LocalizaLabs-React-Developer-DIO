35e4d903b467e6f827e317521129992790f0b804
CJBiohacker <cj.moyses@gmail.com>
2021-09-09 22:48:47 -0300

    "Componente Botão Like"

---
+++ React/Trabalhando com Componentes em React/react-comps-dio/src/components/ButtonThumbUp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ButtonThumbUp = (props) => {
+
+    const { text, children, onClick, className } = props
+
+
+    return (
+        <button type="button" onClick={onClick} class="btn btn-outline-success" >{text}
+            &nbsp;<img src="https://img.icons8.com/material-rounded/24/000000/facebook-like.png" />&nbsp;{children}</button >
+    )
+
+}
+
+export default ButtonThumbUp
+
