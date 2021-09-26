565d13725f9024bbc45e2e722007d5e455baccfc
CJBiohacker <cj.moyses@gmail.com>
2021-09-25 21:04:12 -0300

    "Teste/Desafio avulso Job"

---
+++ teste.js
@@ -0,0 +1,32 @@
+const array = [3, 5, -4, 8, 11, 1, -1, 6];
+// const targetSum = 10;
+
+// const getSumArrayNumberNoRepeat = (A, B) => {
+//     let numberA = A[Math.floor(Math.random() * A.length)];
+//     let numberB = B[Math.floor(Math.random() * B.length)];
+//     let sum = numberA + numberB;
+
+//     while (sum !== 10 && (numberB == numberA)) {
+//         (sum === targetSum && (numberB !== numberA)) ? console.log(sum) : ((numberB = B[Math.floor(Math.random() * B.length)]));
+//     }
+
+//     return console.log([numberA, numberB]);
+// };
+
+// getSumArrayNumberNoRepeat(array, array);
+
+
+const RandomNumberA = (a) => {
+    numberA = a[Math.floor(Math.random() * a.length)];
+
+    return console.log(numberA);
+}
+
+const RandomNumberB = (b) => {
+    numberB = b[Math.floor(Math.random() * b.length)];
+
+    return console.log(numberB);
+}
+
+
+
