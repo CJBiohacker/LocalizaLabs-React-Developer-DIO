b8089f495a80ff23795d63bc645c75061169249b
CJBiohacker <cj.moyses@gmail.com>
2021-09-30 11:42:49 -0300

    "Desafio JavaScript"

--- JavaScript/Desafios/Programa para Validação de Notas.js
+++ JavaScript/Desafios/Programa para Validação de Notas.js
@@ -1,7 +1,27 @@
-const isValid = (value) => value <= 10 && value >= 0;           // Define o intervalo de entrada da nota
+var input = parseInt(gets());
 
-var nota1;
-var nota2;
-var media = (nota1 + nota2) / 2
+const notaValida = input <= 10 && value >= 0;                   // Define as condições de intervalo da entrada da nota. "value <= 10 OU value >= 0"
 
-var conjuntoNotas = [];
+let conjuntoNotas = [];
+
+while (true) {
+
+    if (notaValida) {
+        /*
+            Se o valor for entre 0 a 10, inserir 'i'
+            na matriz 'arr', caso contrário exibir
+            a mensagem de erro do desafio.
+    
+            Se a matriz tiver 2 valores, então: calcular,
+            zerar a matriz e mudar o 'status' para false.
+        */
+    } else {
+        /*
+            Após algum cálculo deverá ser exibida
+            a mensagem de novo cálculo.
+            Daí caso a próxima entrada venha '1', mudar
+            o 'status' para true e se for '2' então
+            o laço while deve ser quebrado.
+        */
+    }
+}
