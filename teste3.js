2eb7cd2813b4fd0e6badcab11f13332687b98c66
CJBiohacker <cj.moyses@gmail.com>
2021-10-05 23:30:46 -0300

    " For Loop e Comparativo de caractere"

--- teste3.js
+++ teste3.js
@@ -6,7 +6,7 @@ ex.: AAA  =>  3A    AABBB => 2A3B
 
 CONDIÇÃO ESPECÍFICA:
 A CADA 10 OU MAIS REPETIÇÕES, OCORRE UM AGRUPAMENTO DO CARACTERE.
-ex.: AAAAAAAAAAAAAAA => 9A6A (9 A + 6 A)   
+ex.: AAAAAAAAAAAAAAA => 9A6A (9 A + 6 A)
 CCCCCCCCCCCCCCCCCCCCCCCCC => 9C9C
 
 ENTRADA
@@ -16,21 +16,49 @@ SAÍDA ESPERADA
 '9B4B2A3C2D'
 */
 
-const string = `BBBBBBBBBBBBBAACCCDD`;
-const arrayString = Array.from(string);
+const string = "BBBBBBBBBBBBBAACCCDD";
 
-const filterArray = (array, param) => {
-    let filterSpecs;
+const comparison = string.match();
 
-    return filterSpecs = array.filter((element) => {
-        return element.indexOf(param);
-    });
-};
+String.fromCharCode(90)                     // Retorna as letras do alfabeto (A-Z), a partir de números ASCII. 
 
-console.log(filterArray(arrayString, 'B'));
+console.log();
+
+for (let i = 65, j = 0; i <= 90, j < string.length; i++, j++) {            // Repetição de índice para Código ASCII e para posição de cada caractere do código em 'string'.
+
+    let nChar = 0;
+
+    if (String.fromCharCode(i) == string.charAt(j)) {
+        nChar++;
+        if (nChar++ < 10) {
+
+        }
+    } else {
+    }
+
+}
+
+// CÓDIGO ASCII A--Z == 65--90 == U+0041--U+005A 
+
 
 
 // CONTADOR DE CARACTERES
 // ALTERAR A STRING ASSIM QUE HOUVER A TRANSIÇÃO DE CARACTERES
 // UTILIZAR INCREMENTO PARA REPETIÇÃO DE CARACTERES
 // CONCATENAR OS CARACTERES APÓS A CONTAGEM
+
+
+// SNIPPET PRA EXCLUIR DO FILTRO UM VALOR ESPECIFICADO DA ARRAY. 
+// const arrayString = Array.from(string);
+
+// console.log(arrayString);
+
+// const filterArray = (array, param) => {
+//     let filterSpecs;
+
+//     return filterSpecs = array.filter((element) => {
+//         return element.indexOf(param);
+//     });
+// };
+
+// console.log(filterArray(arrayString, 'B')); 
