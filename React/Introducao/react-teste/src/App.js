e1095794d5b85d68a1d569dd1810473624655cac
CJBiohacker <cj.moyses@gmail.com>
2021-12-15 19:11:58 -0300

    "App JS"

--- React/Introducao/react-teste/src/App.js
+++ React/Introducao/react-teste/src/App.js
@@ -35,7 +35,7 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Clique nos botões para saber a idade:</p>
 
-        {condition ? ifCJ() : ifTheBoss()}                                       {/* If Ternário para selecionar qual dos botões fica oculto. Condition: True => Botão CJ visível / False => Botão TheBoss visível */}
+        {condition ? ifCJ() : ifTheBoss()}              {/* If Ternário para selecionar qual dos botões fica oculto. Condition: True => Botão CJ visível / False => Botão TheBoss visível */}
 
       </header>
     </div>
